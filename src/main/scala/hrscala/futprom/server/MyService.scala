89d2cf75e2063c72c4fe08d1f4e23c9810d51db5
Marko Elezovic <marko@element.hr>
2015-03-05 21:17:46 +0100

    Worker race (AAA vs BBB vs CCC)

--- src/main/scala/hrscala/futprom/server/MyService.scala
+++ src/main/scala/hrscala/futprom/server/MyService.scala
@@ -1,8 +1,10 @@
 package hrscala.futprom
 package server
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import java.util.Random
+import java.util.concurrent.Executors
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.Actor
 import spray.routing._
@@ -11,8 +13,12 @@ import spray.http._
 import scala.concurrent._
 import scala.collection.mutable.{ LinkedHashMap => MMap }
 
+//import scala.concurrent.ExecutionContext.Implicits.global
+
 class MyServiceActor extends Actor with HttpService {
   def actorRefFactory = context
+
+  private implicit val ec = Worker.ec
 
   def receive = runRoute(
     path("calc-cache" / """\d+""".r) { key =>
@@ -27,20 +33,18 @@ class MyServiceActor extends Actor with HttpService {
     path("calc-no-cache" / """\d+""".r) { key =>
       get {
         complete {
-          Worker.calculateWithoutCache(key.toInt) map { result =>
-            "No cache result: " + result
-          }
+          Worker.calculateWithoutCache(key.toInt)
         }
       }
-    } ~
-    path("clear") {
-      get {
-        complete {
-          Future {
-            Worker.clearCache()
-            "Cleared cache!"
-          }
-        }}
+//    } ~
+//    path("evict") {
+//      get {
+//        complete {
+//          Future {
+//            Worker.clearCache()
+//            "Cleared cache!"
+//          }
+//        }}
     }
   )
 }
@@ -48,30 +52,69 @@ class MyServiceActor extends Actor with HttpService {
 object Worker {
   val random = new Random()
 
-  private def calculateCore(key: Int): Int = {
-    println("Calculating result for: " + key)
+  implicit val ec =
+    ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 
-    val sleepTime = random.nextInt(1000) + 1000
-    Thread.sleep(sleepTime)
 
-    key * 2
+  private def calculateCore(key: Int): String = {
+    println(">>> Calculating result for: " + key)
+    Thread.sleep(1000 * key + random.nextInt(1000))
+    println("<<< Finished calculating result for: " + key)
+    s"Probudio sam se iz mrtvih nakon $key sekundi"
+  }
+
+  private def calculateCore(key: Int, res: Promise[String], name: String): Unit = {
+    println(name + ">>> Calculating result for: " + key)
+    Thread.sleep(random.nextInt(1000))
+
+    var index = key * 10
+    while (index > 1) {
+      index -= 1;
+      Thread.sleep(100)
+      if (res.isCompleted) {
+        println("RES JE COMPLETED, jebes ovo!")
+        index = 0;
+      }
+    }
+    println(name + "rezultat: " + res.trySuccess(s"Probudio sam se iz mrtvih nakon $key sekundi"))
+    println(name + "<<< Finished calculating result for: " + key)
   }
 
   def calculateWithoutCache(key: Int)  = {
-    Future(calculateCore(key))
+    Future {
+      calculateCore(key)
+    }
   }
 
   def calculateWithCache(key: Int) = {
-    if (!(cache contains key)) {
-      cache(key) = Future {
-        calculateCore(key)
+
+      val res = Promise[String]
+      val kandidatZaRacunica = res.future
+
+      val old = cache.putIfAbsent(key, kandidatZaRacunica)
+
+      if (old == null) {
+        Future {
+          calculateCore(key, res, "AAA")
+        }
+
+        Future {
+          calculateCore(key, res, "BBB")
+        }
+
+        Future {
+          calculateCore(key, res, "CCC")
+        }
+
+        kandidatZaRacunica
       }
-    }
+      else {
+        old
+      }
 
-    cache(key)
-  }
+   }
 
-  private val cache = new MMap[Int, Future[Int]]
+  private val cache = new ConcurrentHashMap[Int, Future[String]]
 
   def clearCache() = {
     cache.clear()
